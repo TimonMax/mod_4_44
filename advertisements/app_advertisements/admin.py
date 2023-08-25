285b2412cf5d616344506f63247ff4f65bc6998f
TimonMaxu <timonmaxu@gmail.com>
2023-08-25 12:33:28 +0200

    ДЗ 9 лекции + код 10 лекции

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -3,32 +3,34 @@ from .models import Advertisement
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+
+    list_display = ['id', 'title', 'description', 'price',
+                    'created_date', 'updated_date', 'auction', 'get_html_image']
     list_filter = ['auction', 'created_at']
-    actions = ['make_auction_false', 'make_auction_true']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         (
             'Общее', {
-                'fields': ('title', 'description')
+                'fields': ('user', 'title', 'description', 'image')
             }
         ),
+
         (
             'Финансы', {
                 'fields': ('price', 'auction'),
                 'classes': ['collapse']
             }
         ),
+
     )
 
-    # Замена значения поля торг на нет у выделенных пользователей
-    @admin.action(description='Убрать возможность торга')
-    def make_auction_false(self, request, queryset):
+    # У выделенных обьектов заменяет значение поля "торг" на "нет"
+    @admin.action(description="Убрать возможность торга")
+    def make_auction_as_false(self, request, queryset):
         queryset.update(auction=False)
 
-    # Замена значения поля торг на да у выделенных пользователей
-    @admin.action(description='Добавить возможность торга')
-    def make_auction_true(self, request, queryset):
+    @admin.action(description="Добавить возможность торга")
+    def make_auction_as_true(self, request, queryset):
         queryset.update(auction=True)
 
 admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,49 +1,49 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 class Advertisement(models.Model):
 
-    # товар
-    # Строковое поле небольших размеров
+    user = models.ForeignKey(
+        User,
+        verbose_name='пользователь',
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        'изображение',
+        upload_to='advertisements/'
+    )
+
+    # Товар
+    # строковое поле для небольших размеров
     # 'заголовок' - verbose_name - название поля извне
-    title = models.CharField('Заголовок', max_length=128)
+    title = models.CharField('заголовок', max_length=128)
 
-    # Описание товара
-    #Большое текстовое поле
-    description = models.TextField('Описание')
-
-    # Имя продавца + контакты
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    # Описание товара/информация о товаре
+    # Большое текстовое поле, для больших текстов
+    description = models.TextField('описание')
 
     # Цена
-    # Специальный тип данных с фиксированной запятой
-    description = models.TextField('Описание')
+    # Специальный тип данных с фиксированной точкой
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
 
-    # Уместность торга
-    # Логический тип, два значения
-    auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+    # Уместен ли торг
+    # Логический тип, два значения - правда или ложь
+    auction = models.BooleanField('торг', help_text='Отметьте, уместен ли торг')
 
     # Дата публикации
     # Поле записывается при создании объявления
-    created_at = models.DateTimeField(auto_now_add=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    # Дата изменения
-    # Поле записывается при обновлении объявления
-    updated_at = models.DateTimeField(auto_now=True) 
-
-    # Актуальность объявления
-
-    # Количество товара
-
-
-    # Возможность обмена
-
-    # Адрес продажи/осмотра
-
-    # Б/У товар или нет
-
-    # Возможность взять в долг/рассрочку
+    # Дата изменения/обновления
+    # Поле записывается при каждом обновлении
+    updated_at = models.DateTimeField(auto_now=True)
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -51,24 +51,46 @@ class Advertisement(models.Model):
         if self.created_at.date() == timezone.now().date():
             created_date = self.created_at.strftime("%H:%M:%S")
             return format_html(
-                '<span style="color:green; font-weight:bold;"> Сегодня в {} </span>', 
+                '<span style="color:green; font-weight:bold;">Сегодня в {} </span>',
                 created_date
-                )
+            )
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
-    
+
     @admin.display(description='Дата изменения')
     def updated_date(self):
         from django.utils import timezone
         if self.updated_at.date() == timezone.now().date():
             updated_date = self.updated_at.strftime("%H:%M:%S")
             return format_html(
-                '<span style="color:red; font-weight:bold;"> Сегодня в {} </span>', 
+                '<span style="color:red; font-weight:bold;">Сегодня в {} </span>',
                 updated_date
-                )
+            )
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
-    
+
+    @admin.display(description="Фото")
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{}" style="max-width:80px; max-height:80px"',
+                self.image.url
+            )
+
     def __str__(self):
-        return f"Advertisement(id={self.id}, title=Заголовок {self.title}, price={self.price})"
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
 
     class Meta:
         db_table = 'advertisements'
+
+    # Имя продавца + контакты
+
+    # Актуальность объявления
+
+    # Количество товара
+
+    # Возможен ли обмен
+
+    # Адрес продажи/осмотре
+
+    # Б\У товар или нет
+
+    # Возможность взять в долг/в рассрочку
